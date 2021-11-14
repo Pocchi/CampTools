9faf2456fa58202155fe465fa1df164f41b74be0
Pocchi <pocchi13.tr@gmail.com>
2021-11-14 18:31:44 +0900

    遷移

---
+++ camptools/Controller/ItemInputController.swift
@@ -0,0 +1,32 @@
+//
+//  ItemInputController.swift
+//  camptools
+//  
+//  Created by  on 2021/11/14
+//  
+//
+
+import UIKit
+
+class ItemInputController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ camptools/CustomClass/TextField.swift
@@ -0,0 +1,15 @@
+//
+//  UITextView.swift
+//  camptools
+//  
+//  Created by  on 2021/11/14
+//  
+//
+
+import UIKit
+
+class TextField: UITextField {
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 0
+    }
+}
