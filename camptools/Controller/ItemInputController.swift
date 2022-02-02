aacc4b25a7593fad8fa011255bff87661f8b66d9
Pocchi <pocchi13.tr@gmail.com>
2022-02-02 09:34:04 +0900

    wip input

--- camptools/Controller/ItemInputController.swift
+++ camptools/Controller/ItemInputController.swift
@@ -8,7 +8,7 @@
                                 
 import UIKit
 
-class ItemInputController: UIViewController {
+class ItemInputController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var TextInputStackView: TextStackView! {
         didSet {
             TextInputStackView.borderY()
@@ -24,28 +24,37 @@ class ItemInputController: UIViewController {
             addButtonLabel.tintColor = UIColor.init(named: "disabledText")
         }
     }
-    @IBOutlet weak var MemoTextField: TextField!
-    var name: String?
-    var mamo: String?
+    @IBOutlet weak var memoTextView: UITextView!
+    @IBOutlet weak var memoLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        memoTextView.delegate = self
     }
-    
 
     @IBAction func nameInput(_ sender: UITextField) {
-        addButtonLabel.tintColor = UIColor.white
-        name = sender.text
-        
+        // nameが入力されたらdisabledカラーをはずす
+        if nameTextField.text == nil {
+            addButtonLabel.tintColor = UIColor.init(named: "disabledText")
+        } else {
+            addButtonLabel.tintColor = UIColor.white
+        }
     }
     
-    @IBAction func memoInput(_ sender: UITextField) {
-        mamo = sender.text
+    func textViewDidChange(_ textView: UITextView) {
+        // textViewのプレースホルダを非表示にする
+        if textView.text.count > 0 {
+            memoLabel.isHidden = true
+        } else {
+            memoLabel.isHidden = false
+        }
     }
     
     @IBAction func handleClickAddItem(_ sender: Any) {
-        
+        if nameTextField.text == nil {
+            return
+        }
+        print(memoTextView.text)
+        print(nameTextField.text)
     }
 }
