4d3b7cddf2b184f1b6d30b66057458490b297bd2
Pocchi <pocchi13.tr@gmail.com>
2022-02-02 10:26:30 +0900

    name部分のrx化

--- camptools/Module/ItemInput/ItemInputController.swift
+++ camptools/Module/ItemInput/ItemInputController.swift
@@ -5,7 +5,9 @@
 //  Created by  on 2021/11/14
 //  
 //
-                                
+
+import RxCocoa
+import RxSwift
 import UIKit
 
 class ItemInputController: UIViewController, UITextViewDelegate {
@@ -26,19 +28,25 @@ class ItemInputController: UIViewController, UITextViewDelegate {
     }
     @IBOutlet weak var memoTextView: UITextView!
     @IBOutlet weak var memoLabel: UILabel!
+    private let disposeBag = DisposeBag()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         memoTextView.delegate = self
+        bindingRx()
     }
-
-    @IBAction func nameInput(_ sender: UITextField) {
+    
+    private func bindingRx() {
         // nameが入力されたらdisabledカラーをはずす
-        if nameTextField.text == nil {
-            addButtonLabel.tintColor = UIColor.init(named: "disabledText")
-        } else {
-            addButtonLabel.tintColor = UIColor.white
-        }
+        nameTextField.rx
+            .text.subscribe(onNext: { [weak self] text in
+                if text == nil || text == "" {
+                    self?.addButtonLabel.tintColor = UIColor.init(named: "disabledText")
+                } else {
+                    self?.addButtonLabel.tintColor = UIColor.white
+                }
+            }).disposed(by: disposeBag)
     }
     
     func textViewDidChange(_ textView: UITextView) {
